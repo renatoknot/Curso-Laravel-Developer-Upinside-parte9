cc98dc3e11045008398cd8219af8aa4c014687c0
Renato Gonçalves <renatoslip@hotmail.com>
2020-03-31 12:28:54 -0300

    Fatiando o Layout e controlando rotas

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -8,6 +8,26 @@ class WebController extends Controller
 {
     public function home()
     {
-        echo "Home";
+        return view('front.home');
+    }
+
+    public function course()
+    {
+        return view('front.course');
+    }
+
+    public function blog()
+    {
+        return view('front.blog');
+    }
+
+    public function article()
+    {
+        return view('front.article');
+    }
+
+    public function contact()
+    {
+        return view('front.contact');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'WebController@home')->name('home');
+
+Route::get('/curso', 'WebController@course')->name('course');
+
+Route::get('/blog', 'WebController@blog')->name('blog');
+
+Route::get('/blog/{uri}', 'WebController@article')->name('article');
+
+Route::get('/contato', 'WebController@contact')->name('contact');
