8c243acdcf26fdfa48c4dc2bcf91b397ad78313c
Renato Gonçalves <renatoslip@hotmail.com>
2020-04-01 16:53:58 -0300

    SEO com componente externo coffeecode/optimizer

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Support\Seo;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,11 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $seo;
+
+    public function __construct()
+    {
+        $this->seo = new Seo();
+    }
 }
--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -2,32 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Support\Seo;
+use CoffeeCode\Optimizer\Optimizer;
 use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
     public function home()
     {
-        return view('front.home');
+        $head = $this->seo->render(
+            env('APP_NAME') . ' - UpInside Treinamentos',
+            'A escola eleita a melhor do Brasil com reconhecimento em mais de 17 países pela Latin American Quality Institute',
+            url('/'),
+            asset('images/img_bg_1.jpg')
+        );
+
+        return view('front.home', [
+            'head' => $head
+        ]);
     }
 
     public function course()
     {
-        return view('front.course');
+        $head = $this->seo->render(
+            env('APP_NAME') . ' - Sobre o curso',
+            'Um treinamento completo do zero ao developer com Laravel para você aprender e se especializar no Framework PHP mais utilizado do mundo e abrir as portas de um mercado repleto de oportunidades!',
+            route('course'),
+            asset('images/img_bg_1.jpg')
+        );
+
+        return view('front.course', ['head' => $head]);
     }
 
     public function blog()
     {
-        return view('front.blog');
+        $head = $this->seo->render(
+            env('APP_NAME') . ' - UpInside Treinamentos',
+            'A escola eleita a melhor do Brasil com reconhecimento em mais de 17 países pela Latin American Quality Institute',
+            route('blog'),
+            asset('images/img_bg_1.jpg')
+        );
+
+        return view('front.blog', ['head' => $head]);
     }
 
     public function article()
     {
-        return view('front.article');
+        $head = $this->seo->render(
+            env('APP_NAME') . ' - UpInside Treinamentos',
+            'A escola eleita a melhor do Brasil com reconhecimento em mais de 17 países pela Latin American Quality Institute',
+            route('article'),
+            asset('images/img_bg_1.jpg')
+        );
+
+        return view('front.article', ['head' => $head]);
     }
 
     public function contact()
     {
-        return view('front.contact');
+        $head = $this->seo->render(
+            env('APP_NAME') . ' - UpInside Treinamentos',
+            'A escola eleita a melhor do Brasil com reconhecimento em mais de 17 países pela Latin American Quality Institute',
+            route('contact'),
+            asset('images/img_bg_1.jpg')
+        );
+
+        return view('front.contact', ['head' => $head]);
     }
 }
